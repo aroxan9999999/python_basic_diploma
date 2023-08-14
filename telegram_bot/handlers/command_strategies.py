fd3403f40eab769cc28fe85e954f32ab9e42bc3d
aroxan <aroxan.999@mail.ru>
2023-08-14 21:11:50 +0400

    update

--- telegram_bot/handlers/command_strategies.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from aiogram.types import Message
-
-
-class CommandStrategy(ABC):
-    @abstractmethod
-    async def execute(self, message: Message):
-        pass
--- telegram_bot/handlers/history_handler.py
+++
@@ -1,9 +0,0 @@
-from aiogram.dispatcher import FSMContext
-from states.user_states import UserState
-from .command_strategies import CommandStrategy, Message
-
-
-class HistoryCommandStrategy(CommandStrategy):
-    async def execute(self, message: Message, state: FSMContext):
-        user_state = UserState(state)
-        await user_state.waiting_for_history.set()
